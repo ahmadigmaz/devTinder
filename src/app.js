e8ae987aad6dd92bcd781a4e71b36399df37b0d3
ahmadigmaz <ahmadigmaz@gmail.com>
2025-04-15 07:18:34 +0530

    HTTPS different request

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req,res)=>{
-    res.send("hello hello hello world i am comming by nodemon command");
+app.get("/user",(req,res)=>{
+    res.send({firstname:"igmaz",lastname:"Ahmad"});
 });
 
+app.post("/user",(req,res)=>{
+    //logic to send the data to the database
+    res.send("the database is successfully saved to the database");
+})
+
+app.delete('/user',(req,res)=>{
+    res.send("delete the data from the db");
+})
+
+// use ,method is used to fetch all types of HTTPS request;
 app.use("/test",(req,res)=>{
-    res.send("hello world");
+    res.send("hello world!!!!!!!!");
 });
 
 app.listen(3000,()=>{
