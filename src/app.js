065a4977315a212f3e5225b16ca22e6783e007c6
ahmadigmaz <ahmadigmaz@gmail.com>
2025-04-17 10:13:14 +0530

    middlewared

--- src/app.js
+++ src/app.js
@@ -2,23 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userID/:name/:pass",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname:"igmaz",lastname:"Ahmad"});
-});
-
-app.post("/user",(req,res)=>{
-    //logic to send the data to the database
-    res.send("the database is successfully saved to the database");
-})
-
-app.delete('/user',(req,res)=>{
-    res.send("delete the data from the db");
-})
-
 // use ,method is used to fetch all types of HTTPS request;
-app.use("/test",(req,res)=>{
-    res.send("hello world!!!!!!!!");
+app.use("/test",
+(req,res,next)=>{
+    console.log("response of the first");
+   // res.send("response-1");
+    next();
+},
+(req,res,next)=>{
+    console.log("response of the second");
+    //res.send("response-2");
+    next();
+},
+(req,res)=>{
+    console.log("rH third");
+    res.send("response-3");
 });
 
 app.listen(3000,()=>{
